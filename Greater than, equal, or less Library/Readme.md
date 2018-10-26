2711ac9c2b75aa947efeff07fa74c4f94a4f2652
vinaynv987@gmail.com <vinaynv987@gmail.com>
2018-10-26 01:18:30 -0400

    Changes in library

---
+++ Greater than, equal, or less Library/Readme.md
@@ -0,0 +1,4 @@
+
+
+
+- Build: In Progress
--- Greater than, equal, or less Library/module.py
+++ Greater than, equal, or less Library/module.py
@@ -15,24 +15,13 @@ class ValueComparsions:
             return str(self.val1) + ' is equal to ' + str(self.val2)
         else:
             return str(self.val1) + ' not equal ' + str(self.val2)
-'''
+
     def Lesser(self):
         if self.val1<self.val2:
             return str(self.val1) + ' is lesser than ' + str(self.val2)
         else:
             return str(self.val2) + ' is lesser than ' + str(self.val1)
 
-'''
-class lesser(ValueComparsions):
 
-    def __init__(self, ValueOne, ValueTwo):
-        ValueComparsions.__init__(ValueOne, ValueTwo)
-
-
-    def less(self):
-        if self.val1 < self.val2:
-            return str(self.val1) + ' is lesser than ' + str(self.val2)
-        else:
-            return str(self.val2) + ' is lesser than ' + str(self.val1)
 
 
