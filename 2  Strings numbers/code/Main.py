551ac8151b16632c2a17b723b0bc74eeb423505e
vinaynv987@gmail.com <vinaynv987@gmail.com>
2018-10-26 19:01:12 -0400

    2 strings

--- 2  Strings numbers/code/Main.py
+++ 2  Strings numbers/code/Main.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     try:
         str1 = input('Enter a string Number 1\n')
         str2 = input('Enter a string Number 2 \n')
-        result = ValueComparsions(str1,str2)
+        result = ValueComparsions(str1, str2)
         if str1 == str2:
             print(result.equal())
-            print('{0} and {1} are not greater or \nlesser to each other'.format(str1,str2))
+            print('{0} and {1} are not greater or \nlesser to each other'.format(str1, str2))
         else:
             print(result.greater())
             print(result.equal())
